8c112615732cbe2845299ab54cbb9c20184cebf3
somaryam <somaryam01@gmail.com>
2024-04-20 14:22:34 +0000

    message commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,34 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		logger.info("Car App started ! Goto http://localhost:8081/dic_student_name");
+	}
+
 }
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/dic_student_name")
 class DemoController {
+	@Value("${fullname: Mariama Sow}")
+	private String fullname;
 
-    @Value("${fullname:Mariama}")
-    private String fullname;
-
-    @GetMapping
-    String getFullname() {
-        return "Hello World, I'm " + fullname + "Sow from M1GLSI!";
-    }
+	@GetMapping
+	String getFullname() {
+		return "Hello world, my name is  " + this.fullname + "!";
+	}
 }
